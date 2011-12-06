a52438a5851499b13b048a4ccac7635057651ff3
Jesper Joergensen <jesper@heroku.com>
2011-12-06 10:50:27 -0800

    Removed debug output

--- src/play/modules/force/ForceAuthPlugin.java
+++ src/play/modules/force/ForceAuthPlugin.java
@@ -7,13 +7,7 @@ public class ForceAuthPlugin extends PlayPlugin {
 
 	@Override
 	public void onApplicationStart() {
-		System.out.println("ForceAuthPlugin called");
 		Router.addRoute("GET", "/_auth", "controllers.force.Authentication.index");
 	}	
 	
-	@Override
-	public boolean rawInvocation(Http.Request request, Http.Response response) {
-		System.out.println("Processing request: "+request.url);
-		return false;
-	}
 }
